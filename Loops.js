a97bb72204bab88dfd0198b1ef4e4344d05b571c
zalex7934-creator <zalex7934@gmail.com>
2026-09-24 21:03:18 -0400

    ewfewfwfwef
    fewfewf
    
    hfg

--- Loops.js
+++ Loops.js
@@ -12,14 +12,13 @@
 // Use a for loop and .push() to build the array one number at a time.
 function getNumbersInRange(start, end) {
   // TODO: your code here
-
-  for (let start ; start< end; start++) {
-    myArray.push(start);
-    return myArray
-  
-}
+let result= []
+for(let i = start;i <= end; i++){result.push(i);}
+return result;
 
 }
+g
+
 
 console.log(getNumbersInRange(1, 5));  // [1, 2, 3, 4, 5]
 console.log(getNumbersInRange(10, 10)); // [10]
